76b8f1dd13a35c4f3080f9cabf862ce5ae8340fe
mekis <72376854+budaizalan@users.noreply.github.com>
2025-02-28 07:58:16 +0100

    Update databaseConnection.test.js

--- backend/tests/databaseConnection.test.js
+++ backend/tests/databaseConnection.test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 describe("Connect to real database", () => {
     test("Connection succeeded", async () => {
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT;
         const MONGODB_URI = process.env.MONGODB_URI;
 
         if (!MONGODB_URI) console.error("No database URL defined");
