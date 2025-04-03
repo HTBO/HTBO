0b1c36012524550e497b338a30e9e333fb97a116
pallanghunor <pallang.hunor@students.jedlik.eu>
2025-04-03 13:22:41 +0200

    hide sidebar on smaller screens

--- frontend/app/components/Menu/Sidebar.vue
+++ frontend/app/components/Menu/Sidebar.vue
@@ -4,7 +4,7 @@ import { menu } from '~/constants/Menu'
 </script>
 
 <template>
-    <div class="sticky top-0 w-64 h-full flex-none flex flex-col justify-between p-6 bg-surface-900">
+    <div class="max-lg:hidden sticky top-0 w-64 h-full flex-none flex flex-col justify-between p-6 bg-surface-900">
         <div class="flex flex-col gap-10">
             <span class="text-xl font-bold">HTBO</span>
             <MenuGroup v-for="group in menu" :key="group.title" :group="group"/>
