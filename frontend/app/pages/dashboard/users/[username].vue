c8b41f27090336cb367caa71fdd445a8d06554e0
pallanghunor <pallang.hunor@students.jedlik.eu>
2025-04-07 14:32:48 +0200

    Refactor FriendsSection and Sessions components; implement friend fetching logic and add fade transition effect

--- frontend/app/pages/dashboard/users/[username].vue
+++ frontend/app/pages/dashboard/users/[username].vue
@@ -14,49 +14,50 @@ const { username } = route.params as { username: string }
 
 const { data: user, error, status } = fetchUserByUsername(username);
 
-console.log(user.value)
-
 const addFriend = () => { };
 
 const activeTab = ref<Tab | null>(profileTabs[0] ?? null)
 </script>
 
 <template>
-    <div class="flex flex-col gap-5">
-        <div>
-            <div class="h-80 mx-5 bg-gray-700 rounded-2xl">
-                <img src="/banner.jpg" width="5376" height="3072" alt="Banner" class="h-full w-full object-cover rounded-2xl" />
-            </div>
-            <div class="-mt-12 h-24 flex justify-center p-5 px-10 bg-gray-800/50 drop-shadow-xl inset-shadow-sm inset-shadow-gray-700/50 backdrop-blur-md rounded-xl">
-                <div class="flex-1 flex">
-                    <div class="flex flex-col items-center">
-                        <p class="text-lg font-semibold">{{ user?.friends.length }}</p>
-                        <p class="font-lg font-semibold">Friends</p>
+    <Transition name="fade">
+        <div v-if="status === 'success'" class="flex flex-col gap-5">
+            <div>
+                <div class="h-80 mx-5 bg-gray-700 rounded-2xl">
+                    <img src="/banner.jpg" width="5376" height="3072" alt="Banner" class="h-full w-full object-cover rounded-2xl" />
+                </div>
+                <div class="-mt-12 h-24 flex justify-center p-5 px-10 bg-gray-800/50 drop-shadow-xl inset-shadow-sm inset-shadow-gray-700/50 backdrop-blur-md rounded-xl">
+                    <div class="flex-1 flex">
+                        <div class="flex flex-col items-center">
+                            <p class="text-lg font-semibold">{{ user?.friends.length }}</p>
+                            <p class="font-lg font-semibold">Friends</p>
+                        </div>
+                    </div>
+                    <div class="flex-none flex flex-col items-center gap-2 -mt-32 z-1">
+                        <div class="size-32 bg-gray-800 border-4 border-primary-600 drop-shadow-2xl rounded-full">
+                            <NuxtImg :src="user?.avatarUrl" class="group-hover:opacity-70 rounded-full duration-300" />
+                        </div>
+                        <p class="text-2xl font-semibold">{{ user?.username }}</p>
+                    </div>
+                    <div class="flex-1 flex justify-end items-center">
+                        <ClientOnly>
+                            <NuxtLink v-if="authStore.user?._id === user?._id" to="/dashboard" class="flex items-center gap-2 bg-primary-600 hover:bg-primary-700 font-semibold py-2 px-4 rounded-lg duration-300">
+                                <Icon name="icons:edit" size="1.25rem" />
+                                <p>Edit Profile</p>
+                            </NuxtLink>
+                            <button @click="addFriend" v-else class="flex items-center gap-2 bg-primary-600 hover:bg-primary-700 font-semibold py-2 px-4 rounded-lg duration-300">
+                                <Icon name="icons:add" size="1.25rem" />
+                                Add Friend
+                            </button>
+                        </ClientOnly>
                     </div>
                 </div>
-                <div class="flex-none flex flex-col items-center gap-2 -mt-32 z-1">
-                    <div class="size-32 bg-gray-800 border-4 border-primary-600 drop-shadow-2xl rounded-full">
-                        <NuxtImg :src="user?.avatarUrl" class="group-hover:opacity-70 rounded-full duration-300" />
-                    </div>
-                    <p class="text-2xl font-semibold">{{ user?.username }}</p>
-                </div>
-                <div class="flex-1 flex justify-end items-center">
-                    <ClientOnly>
-                        <NuxtLink v-if="authStore.user?._id === user?._id" to="/dashboard" class="flex items-center gap-2 bg-primary-600 hover:bg-primary-700 font-semibold py-2 px-4 rounded-lg duration-300">
-                            <Icon name="icons:edit" size="1.25rem" />
-                            <p>Edit Profile</p>
-                        </NuxtLink>
-                        <button @click="addFriend" v-else class="flex items-center gap-2 bg-primary-600 hover:bg-primary-700 font-semibold py-2 px-4 rounded-lg duration-300">
-                            <Icon name="icons:add" size="1.25rem" />
-                            Add Friend
-                        </button>
-                    </ClientOnly>
-                </div>
             </div>
+            <TabNavigation :tabs="profileTabs" @update:active-tab="activeTab = $event"/>
+            <component :is="activeTab?.component" :user="user"/>
         </div>
-        <TabNavigation :tabs="profileTabs" @update:active-tab="activeTab = $event"/>
-        <component :is="activeTab?.component" :user="user"/>
-    </div>
+    </Transition>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
